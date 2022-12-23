edecabeb46faf530b4f67db5e4240b2b39038c72
Jeremy Woertink <jeremywoertink@gmail.com>
2022-12-22 20:57:14 -0800

    adding entry detail parsing

--- spec/nacha/entry_detail_spec.cr
+++ spec/nacha/entry_detail_spec.cr
@@ -19,4 +19,32 @@ describe Nacha::EntryDetail do
       io.to_s.should eq(example)
     end
   end
+
+  describe "parse" do
+    it "parses the data correctly" do
+      line = "6270210000211230000099       00010000003213211234     DARON BERGSTROM       S 0012345678912345"
+      entry_detail = Nacha::EntryDetail.parse(line)
+      entry_detail.transaction_code.should eq(Nacha::EntryDetail::TransactionCode::CheckingDebit)
+      entry_detail.dfi_routing_number.should eq("021000021")
+      entry_detail.dfi_account_number.should eq("1230000099")
+      entry_detail.amount.should eq(1000000)
+      entry_detail.individual_name.should eq("DARON BERGSTROM")
+    end
+
+    context "errors" do
+      it "raises when it's not a BatchHeader" do
+        line = "101 021000021 3210001232209291648D094101WELLS FARGO BANK       ACME CORPORATION               "
+        expect_raises(Nacha::ParserError, "Invalid Type Code '1' for Entry Detail") do
+          Nacha::EntryDetail.parse(line)
+        end
+      end
+
+      it "raises when it's the wrong length" do
+        line = "627021"
+        expect_raises(Nacha::ParserError, "Invalid Record Length '6' for Entry Detail") do
+          Nacha::EntryDetail.parse(line)
+        end
+      end
+    end
+  end
 end
