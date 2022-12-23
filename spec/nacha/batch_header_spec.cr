ceebdf7e73e321f8229ded51dddf17dc187401a0
Jeremy Woertink <jeremywoertink@gmail.com>
2022-12-22 20:08:08 -0800

    adding in batch header parsing

--- spec/nacha/batch_header_spec.cr
+++ spec/nacha/batch_header_spec.cr
@@ -25,4 +25,31 @@ describe Nacha::BatchHeader do
       io.to_s.size.should eq(94)
     end
   end
+
+  describe "parse" do
+    it "parses the data correctly" do
+      line = "5200ACME CORPORATION                    1233211212WEBONLINEPYMT2209292209302731012000120000261"
+      batch_header = Nacha::BatchHeader.parse(line)
+      batch_header.service_class_code.should eq(Nacha::Batch::ServiceClassCode::Mixed)
+      batch_header.company_name.should eq("ACME CORPORATION")
+      batch_header.standard_entry_class.should eq(Nacha::BatchHeader::StandardEntryClass::WEB)
+      batch_header.company_entry_description.should eq("ONLINEPYMT")
+    end
+
+    context "errors" do
+      it "raises when it's not a BatchHeader" do
+        line = "6270210000211230000099       00010000003213211234     DARON BERGSTROM       S 0012345678912345"
+        expect_raises(Nacha::ParserError, "Invalid Type Code '6' for Batch Header") do
+          Nacha::BatchHeader.parse(line)
+        end
+      end
+
+      it "raises when it's the wrong length" do
+        line = "5200"
+        expect_raises(Nacha::ParserError, "Invalid Record Length '4' for Batch Header") do
+          Nacha::BatchHeader.parse(line)
+        end
+      end
+    end
+  end
 end
--- src/nacha/batch_header.cr
+++ src/nacha/batch_header.cr
@@ -5,6 +5,7 @@ module Nacha
   # debits in this batch.
   class BatchHeader
     include BuildableRecord
+    include ParsableRecord
 
     TYPE_CODE = 5
 
@@ -16,24 +17,80 @@ module Nacha
       WEB # Authorization received via the Internet
     end
 
-    getter service_class_code : Batch::ServiceClassCode
-    getter company_identification : String
-    getter originating_dfi_identification : String
-    getter batch_number : Int32
+    getter service_class_code : Batch::ServiceClassCode # Credit, Debit, Mixed
+    getter company_name : String                        # Same as immediate_origin_name in the FileHeader
+    getter company_identification : String              # This should be the immediate_origin from the FileHeader or a custom designation
+    getter standard_entry_class : StandardEntryClass    # Type reason for the money transfer
+    getter company_entry_description : String           # Description of the transaction to be printed on the receivers' bank statement
+    getter effective_entry_date : Time                  # The date to post the transaction
+    getter originating_dfi_identification : String      # Same as immediate_destination in FileHeader
+    getter originator_status_code : Char                # The ODFI initiating the entry.
+    getter company_discretionary_data : String?         # For your custom accounting purposes
+    getter company_descriptive_date : Time?             # The date to print on the receivers' bank statement
+    getter batch_number : Int32                         # Increment this for each batch
 
     def initialize(
-      @service_class_code : Batch::ServiceClassCode, # Credit, Debit, Mixed
-      @company_name : String,                        # Same as immediate_origin_name in the FileHeader
-      @company_identification : String,              # This should be the immediate_origin from the FileHeader or a custom designation
-      @standard_entry_class : StandardEntryClass,    # Type reason for the money transfer
-      @company_entry_description : String,           # Description of the transaction to be printed on the receivers' bank statement
-      @effective_entry_date : Time,                  # The date to post the transaction
-      @originating_dfi_identification : String,      # Same as immediate_destination in FileHeader
-      @originator_status_code : Char,                # The ODFI initiating the entry.
-      @company_discretionary_data : String? = nil,   # For your custom accounting purposes
-      @company_descriptive_date : Time? = nil,       # The date to print on the receivers' bank statement
-      @batch_number : Int32 = 1                      # Increment this for each batch
+      @service_class_code : Batch::ServiceClassCode,
+      @company_name : String,
+      @company_identification : String,
+      @standard_entry_class : StandardEntryClass,
+      @company_entry_description : String,
+      @effective_entry_date : Time,
+      @originating_dfi_identification : String,
+      @originator_status_code : Char,
+      @company_discretionary_data : String? = nil,
+      @company_descriptive_date : Time? = nil,
+      @settlement_date : Int32? = nil,
+      @batch_number : Int32 = 1
     )
+    end
+
+    def self.raise_parse_error(name : String, value : String)
+      ParsableRecord.raise_parse_failed_error(name, value, "Batch Header")
+    end
+
+    def self.parse(input : String) : self
+      if input.bytesize == Nacha::File::RECORD_SIZE
+        type_code = input[0].to_s
+        type_code == TYPE_CODE.to_s || raise_parse_error("Type Code", type_code)
+
+        service_class_code = input[1..3]
+        service_class_code.match(/\d+/) || raise_parse_error("Service Class Code", service_class_code)
+
+        company_name = input[4..19]
+        company_discretionary_data = input[20..39]
+        company_identification = input[40..49]
+        standard_entry_class = input[50..52]
+        company_entry_description = input[53..62]
+        company_descriptive_date = input[63..68]
+
+        effective_entry_date = input[69..74]
+        effective_entry_date.match(/\d{6}/) || raise_parse_error("Effective Entry Date", effective_entry_date)
+
+        settlement_date = input[75..77]
+        settlement_date.match(/(\s{3}|\d{3})/) || raise_parse_error("Settlement Date", settlement_date)
+
+        originator_status_code = input[78]
+        originating_dfi_identification = input[79..86]
+        batch_number = input[87..93]
+        batch_number.match(/\d+/) || raise_parse_error("Batch Number", batch_number)
+
+        new(
+          service_class_code: Batch::ServiceClassCode.from_value(service_class_code.to_i),
+          company_name: company_name,
+          company_identification: company_identification,
+          standard_entry_class: StandardEntryClass.parse(standard_entry_class),
+          company_entry_description: company_entry_description,
+          effective_entry_date: Time.parse_utc(effective_entry_date, "%y%m%d"),
+          originating_dfi_identification: originating_dfi_identification,
+          originator_status_code: originator_status_code,
+          company_discretionary_data: company_discretionary_data.strip.presence,
+          company_descriptive_date: company_descriptive_date.strip.presence.try { |val| Time.parse_utc(val, "%y%m%d") },
+          batch_number: batch_number.to_i,
+        )
+      else
+        raise_parse_error("Record Length", input.bytesize.to_s)
+      end
     end
 
     def build(io : IO) : IO
@@ -46,7 +103,7 @@ module Nacha
       io << @company_entry_description.ljust(10, ' ')
       io << @company_descriptive_date.try(&.to_s("%y%m%d")).to_s.ljust(6, ' ')
       io << @effective_entry_date.to_s("%y%m%d")
-      io << "   "
+      io << @settlement_date.to_s.ljust(3, ' ')
       io << @originator_status_code.to_s
       io << @originating_dfi_identification.to_s.rjust(8, ' ')
       io << @batch_number.to_s.rjust(7, '0')
